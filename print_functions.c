8a02c0ce7a35ba96ac6ee4caa4f6ece156813481
ptjohns2 <ptjohns2@asu.edu>
2014-02-21 17:45:28 -0700

    Updated line maxlength alg

--- print_functions.c
+++ print_functions.c
@@ -9,27 +9,29 @@ static void print_page_header(char source_name[], char date[]);
 void print_line(char *line, char source_name_to_print[], char date_to_print[]){
 	//Initialize local variables
 	char save_ch = '\0';
-	char *save_chp = NULL;
 	static int line_count = MAX_LINES_PER_PAGE;
-		   
-	line_count++;
-	    
-	if (line_count > MAX_LINES_PER_PAGE)
+	
+	//Print page header is number of lines printed is more than one page
+	if ((++line_count)  > MAX_LINES_PER_PAGE)
 	{
 		print_page_header(source_name_to_print, date_to_print);
 		line_count = 1;
 	}
-	    
+	 
+	//this should stop lines from printing longer than 80 characters...
+	//but '\t' is one character which in the console takes up more
+	//then one character icon, so sometimes there is spillage   
 	if (sizeof(line) > MAX_PRINT_LINE_LENGTH){
-		save_chp = &line[sizeof(line)-1];
+		save_ch = line[MAX_PRINT_LINE_LENGTH];
+		line[MAX_PRINT_LINE_LENGTH] = '\0';
 	}
-    
-	if (save_chp != NULL){
-		save_ch = *save_chp;
-		save_chp = '\0';
-	}
-    
+    	//print line
 	printf(line);
+
+	//restore line to unconcatenated form (replace the overwritten '\0')
+	if (save_ch != '\0'){
+		line[MAX_PRINT_LINE_LENGTH] = save_ch;
+	}
 }
 
 
